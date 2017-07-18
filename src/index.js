acaa6072604878a65886e98c3fad7d61c95c8a27
bobwei <bob61016@gmail.com>
2017-07-18 23:04:04 +0800

    add getCredentials

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ export * from './actions';
 export { default as createInitialState } from './createInitialState';
 export { default as createReducer } from './createReducer';
 export { default as isAuthenticated } from './selectors/isAuthenticated';
+export { default as getCredentials } from './selectors/getCredentials';
 export { default as requireAuth } from './requireAuth';
